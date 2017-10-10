3363a509d80a62c77c1ecead1fb4fe2f2b21b38b
vwctb <taebumchoi@vwctbui-MacBook-Pro.local>
2017-10-10 16:51:29 +0900

    remain2.0 0.2v

---
+++ src/server/index.js
@@ -0,0 +1,14 @@
+const Koa = require('koa');
+const serve = require('koa-static');
+const path = require('path');
+const fs = require('fs');
+const app = new Koa();
+
+const indexHtml = fs.readFileSync(path.resolve(__dirname, '../build/index.html'), { encoding: 'utf8' });
+
+app.use(serve(path.resolve(__dirname, '../build/')));
+app.use(ctx => {
+  ctx.body = indexHtml;
+});
+
+app.listen(3001);
