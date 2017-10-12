3f8f57df4e4f81c7ef16827f2ba78d857a7d7826
vwctb <taebumchoi@vwctbui-MacBook-Pro.local>
2017-10-12 10:20:02 +0900

    0.4v

--- server/index.js
+++ server/index.js
@@ -5,10 +5,13 @@ const fs = require('fs');
 const app = new Koa();
 
 const indexHtml = fs.readFileSync(path.resolve(__dirname, '../build/index.html'), { encoding: 'utf8' });
+const port = process.env.PORT || 4000;
 
 app.use(serve(path.resolve(__dirname, '../build/')));
 app.use(ctx => {
   ctx.body = indexHtml;
 });
 
-app.listen(80);
+app.listen(port, () => {
+  console.log('heurm server is listening to port ' + port);
+});
