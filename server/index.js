5264a62e9dcba2e7543fe1ba9a6bb257395607c7
vwctb <taebumchoi@vwctbui-MacBook-Pro.local>
2017-10-12 10:34:35 +0900

    0.5v

--- server/index.js
+++ server/index.js
@@ -5,13 +5,11 @@ const fs = require('fs');
 const app = new Koa();
 
 const indexHtml = fs.readFileSync(path.resolve(__dirname, '../build/index.html'), { encoding: 'utf8' });
-const port = process.env.PORT || 4000;
-
 app.use(serve(path.resolve(__dirname, '../build/')));
 app.use(ctx => {
   ctx.body = indexHtml;
 });
 
-app.listen(port, () => {
-  console.log('raemian server is listening to port ' + port);
+app.listen(80, () => {
+  console.log('raemian server is listening to port 80');
 });
