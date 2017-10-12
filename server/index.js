a01ed6125763a61473a17f7ce39d2721daf34b7a
vwctb <taebumchoi@vwctbui-MacBook-Pro.local>
2017-10-12 13:26:51 +0900

    0.7v

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const Koa = require('koa');
 const serve = require('koa-static');
 const path = require('path');
 const fs = require('fs');
+const logger = require('koa-logger')
 const app = new Koa();
 
 const indexHtml = fs.readFileSync(path.resolve(__dirname, '../build/index.html'), { encoding: 'utf8' });
@@ -9,6 +10,7 @@ app.use(serve(path.resolve(__dirname, '../build/')));
 app.use(ctx => {
   ctx.body = indexHtml;
 });
+app.use(logger());
 
 app.listen(80, () => {
   console.log('raemian server is listening to port 80');
