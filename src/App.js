bc088457d8528289d03431c3e20db59703e08eaa
vwctb <taebumchoi@vwctbui-MacBook-Pro.local>
2017-10-23 14:17:17 +0900

    1.4v

--- src/App.js
+++ src/App.js
@@ -32,7 +32,8 @@ class App extends Component {
         console.log('strAgent',strAgent);
         console.log('window', window);
         console.log('module', module.device);
-        alert('uuid1:'+ module.device.uuid);
+        console.log('uuid1:',module.exports);
+        console.log('uuid2:', module.exports.device.uuid);
         alert('uuid2:'+ module.exports.device.uuid);
 
         if(!history.location.pathname.match('auth')){
