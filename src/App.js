4c728a58af2b906099a8e456e842fc48e54c1f88
vwctb <taebumchoi@vwctbui-MacBook-Pro.local>
2017-10-23 14:05:49 +0900

    1.2v

--- src/App.js
+++ src/App.js
@@ -32,9 +32,8 @@ class App extends Component {
         console.log('strAgent',strAgent);
         console.log('window', window);
         console.log('module', module.device);
-        console.log('uuid1', module.device.uuid);
-        console.log('uuid2', module.exports.device.uuid);
-        alert('module', module.device.uuid);
+        alert('uuid1', module.device.uuid);
+        alert('uuid2', module.exports.device.uuid);
 
         if(!history.location.pathname.match('auth')){
             const { AuthActions } = this.props;
