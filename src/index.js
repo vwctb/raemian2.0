d4adcb0a5cad24ce515a39fbb73497972f5af6cc
vwctb <taebumchoi@vwctbui-MacBook-Pro.local>
2017-11-16 15:01:46 +0900

    7.1v

--- src/index.js
+++ src/index.js
@@ -9,9 +9,14 @@ import configureStore from 'redux/configureStore';
 //import socket from 'lib/socket';
 
 const store = configureStore();
+
+const para = document.location.href.split(":")[1];
+const socketURI = 'http:'+para+':4805'//웹소켓 접속정보
+/*
 const socketURI = process.env.NODE_ENV === 'production' 
                     ? ((window.location.protocol === "https:") ? "wss://" : "ws://") + window.location.host + "/ws"
                     : 'ws://122.199.242.18:4805';
+*/
      // socket.initialize(store, socketURI);
 const render = Component => {
     window.store = store;
